e051d449cd19b8e8d520ce43eb77c4a8dd188710
MD Emon Bhuiyan <109236498+emonbhuiyan@users.noreply.github.com>
2023-09-21 11:54:20 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ opkg update
 opkg install iptables iptables-mod-nat-extra redsocks
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cp redsocks.conf /etc/redsocks.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cp redsocks /etc/init.d/redsocks && chmod +x /etc/init.d/redsocks
-
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/emonbhuiyan/Redsocks-OpenWRT/raw/main/redsocks.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/emonbhuiyan/Redsocks-OpenWRT/raw/main/redsocks && chmod +x /etc/init.d/redsocks
 
 echo -e "Thanks for installing. Follow me for more updates. https://fb.me/emoncontact"
